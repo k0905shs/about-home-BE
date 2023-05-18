6731c9cbc4cd4f8116f3d07de697a2b93fe905ca
k0905sh <k0905sh@kakao.com>
2023-05-19 01:53:50 +0900

    서버 포트 변경 / 빌딩 타입 변경

--- my-home/src/main/java/com/myhome/type/BuildingType.java
+++ my-home/src/main/java/com/myhome/type/BuildingType.java
@@ -7,9 +7,9 @@ import lombok.Getter;
 @Getter
 public enum BuildingType {
     APART("아파트", GovRequestUri.BUILDING_SALE_APART),
-    HOUSE("다세대주택 / 연립주택 / 빌라 / 도시형 생활주택", GovRequestUri.BUILDING_SALE_HOUSE),
+    TOWNHOUSE("다세대주택 / 연립주택 / 빌라 / 도시형 생활주택", GovRequestUri.BUILDING_SALE_TOWNHOUSE),
     OFFICETEL("오피스텔", GovRequestUri.BUILDING_SALE_OFFICETEL),
-    TOWNHOUSE("단독/다가구", GovRequestUri.BUILDING_SALE_TOWNHOUSE);
+    HOUSE("단독/다가구", GovRequestUri.BUILDING_SALE_HOUSE);
 
     private final String desc;
     private final GovRequestUri govRequestUri;
